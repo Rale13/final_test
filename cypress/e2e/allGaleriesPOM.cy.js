5caef1ce643da1e2e1a27f075967608800df3705
Rale13 <rastko.kostic@gmail.com>
2022-06-09 15:44:25 +0200

    create and all galleries

--- cypress/e2e/allGaleriesPOM.cy.js
+++ cypress/e2e/allGaleriesPOM.cy.js
@@ -1,26 +1,13 @@
 /// <reference types="Cypress" />
 
-import {loginPage} from '../page_objects/loginPage'
 import {allGalleries} from '../page_objects/allGaleries'
 
 describe('create POM', () => {
-    
-    const loginCredentials = {
-    validEmail: 'rale@gmail.com',
-    validPass: 'Test1234'
-    }
 
     let filterText = "Test galerija"
 
     before('visit login page', () => {
         cy.visit('/')
-        loginPage.loginBtn.click();
-        cy.url().should('include', '/login');
-        loginPage.login(
-            loginCredentials.validEmail, 
-            loginCredentials.validPass
-            );
-        cy.url().should('not.include', '/login');
         cy.get('.cell').should('have.length', 10);
     })
 
